3beee52f3bff262dd4540b1527060a413fcfbd54
yaxing.chen <chenyaxing@rt-thread.com>
2020-09-13 15:41:35 +0800

    first commit

---
+++ rtt_default_project_0/rtconfig_preinc.h
@@ -0,0 +1,19 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define CPU_MIMXRT1064DVL6A
+#define EVK_MCIMXRM
+#define FSL_SDK_ENABLE_DRIVER_CACHE_CONTROL
+#define HAVE_CCONFIG_H
+#define RT_USING_NEWLIB
+#define SKIP_SYSCLK_INIT
+#define STD C99
+#define XIP_BOOT_HEADER_ENABLE 1
+#define XIP_EXTERNAL_FLASH 1
+#define __START entry
+
+#endif /*RTCONFIG_PREINC_H__*/
