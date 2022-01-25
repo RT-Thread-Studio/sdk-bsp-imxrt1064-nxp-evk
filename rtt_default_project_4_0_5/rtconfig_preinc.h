dec11b1e92d58c8138ed31ea8a371b84d460d813
jiangjianbo <jiangjianbo@rt-thread.com>
2022-01-25 15:18:46 +0800

    Add support for RT-Thread 4.0.5

---
+++ rtt_default_project_4_0_5/rtconfig_preinc.h
@@ -0,0 +1,20 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define CPU_MIMXRT1064DVL6A
+#define EVK_MCIMXRM
+#define FSL_SDK_ENABLE_DRIVER_CACHE_CONTROL 1
+#define HAVE_CCONFIG_H
+#define RT_USING_NEWLIB
+#define SKIP_SYSCLK_INIT
+#define XIP_BOOT_HEADER_DCD_ENABLE 1
+#define XIP_BOOT_HEADER_ENABLE 1
+#define XIP_EXTERNAL_FLASH 1
+#define __RTTHREAD__
+#define __START entry
+
+#endif /*RTCONFIG_PREINC_H__*/
